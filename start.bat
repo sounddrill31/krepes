32a1117bd7cd09f9ca21836453a6b25993ecf06e
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-07-05 05:54:40 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,19 +2,15 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 net config server /srvcomment:"Windows Server By LittleFox" > errormsg.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > errormsg.txt 2>&1
-net user memeqbasah sudoisgod /add >nul
-net localgroup administrators memeqbasah /add >nul
-net user memeqbasah /active:yes >nul
+net user LittleFox 4321?foxy /add >nul
+net localgroup administrators LittleFox /add >nul
+net user LittleFox /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant memeqbasah:F >nul
-ICACLS C:\Windows\installer /grant memeqbasah:F >nul
-echo Successfully installed! If RDP is dead, rebuild again.
+ICACLS C:\Windows\Temp /grant LittleFox:F >nul
+ICACLS C:\Windows\installer /grant LittleFox:F >nul
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: memeqbasah
-echo Password: sudoisgod
-echo You can login now!
 ping -n 10 127.0.0.1 >nul
