0e45e7e6c6b00bf692ac05186d4bb2b1dba89f7b
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-14 07:28:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs --depth=1
 #clone dev tree
 git clone https://github.com/P-Salik/local_manifest --depth 1 -b lineage .repo/local_manifests &&
+
+if [ ! 0 == 0 ]
+
+ then   curl -o .repo/local_manifests https://github.com/P-Salik/local_manifest
+
+   echo Git Clone Failed, downloading through curl instead...
+
+ fi
+ 
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
