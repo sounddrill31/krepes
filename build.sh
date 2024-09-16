74886e2849b5d78a208eb7267876e9eaa152b86a
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-17 03:51:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@ rm -rf .repo/local_manifests/  &&
 # Clone local_manifests repository
 repo init -u https://github.com/ProjectBlaze/manifest.git -b 12.1 --git-lfs --depth=1
 # clone dev tree
-git clone https://github.com/nathannxx/local_manifests_RMX1941.git --depth 1 -b RMX1941 .repo/local_manifests &&
+git clone https://github.com/nathannxx/local_manifest.git --depth 1 -b blaze .repo/local_manifests &&
 
 if [ ! 0 == 0 ]
 
- then   curl -o .repo/local_manifests https://github.com/nathannxx/local_manifests_RMX1941.git
+ then   curl -o .repo/local_manifests https://github.com/nathannxx/local_manifest.git
 
    echo Git Clone Failed, downloading through curl instead...
 
