dc07710f309f8e3ed68fcd27331d443eab400a06
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-17 05:31:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@ rm -rf .repo/local_manifests/  &&
 repo init -u https://github.com/ProjectBlaze/manifest.git -b 12.1 --git-lfs --depth=1
 # clone dev tree
 git clone https://github.com/nathannxx/local_manifest.git --depth 1 -b blaze .repo/local_manifests &&
-
-if [ ! 0 == 0 ]
-
- then   curl -o .repo/local_manifests https://github.com/nathannxx/local_manifest.git
-
-   echo Git Clone Failed, downloading through curl instead...
-
- fi
- 
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
