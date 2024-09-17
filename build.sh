e372ae4c298af5fbfb1b3aef2a9e7cdb636f380e
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-17 16:37:36 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 rm -rf .repo/local_manifests/  &&
 # Clone local_manifests repository
-repo init -u https://github.com/xdroid-oss/xd_manifest -b twelve --git-lfs; \
+repo init -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next --git-lfs
 # Cloning Depis Teri
-git clone https://github.com/nathannxx/android_device_realme_RMX1941 --depth 1 -b twelve device/realme/RMX1941; \
-git clone https://github.com/P-Salik/android_kernel_realme_RMX1941 --depth 1 -b Q-rebase kernel/realme/RMX1941; \
-git clone https://github.com/P-Salik/android_vendor_realme_RMX1941 --depth 1 -b Sv2 vendor/realme/RMX1941; \
+git clone https://github.com/nathannxx/android_device_realme_RMX1941 --depth 1 -b twelve device/realme/RMX1941
+git clone https://github.com/P-Salik/android_kernel_realme_RMX1941 --depth 1 -b Q-rebase kernel/realme/RMX1941
+git clone https://github.com/P-Salik/android_vendor_realme_RMX1941 --depth 1 -b Sv2 vendor/realme/RMX1941
 # Sync the repositories
-/opt/crave/resync.sh; \
-source build/envsetup.sh; \
+/opt/crave/resync.sh
+source build/envsetup.sh
 # Set up build environment
 export BUILD_USERNAME=natehiggas
 export BUILD_HOSTNAME=crave
@@ -15,6 +15,6 @@ export TZ=Asia/Jakarta
 export WITH_GMS=true
  
 # Build the ROM
-breakfast RMX1941 userdebug; \
+breakfast RMX1941 userdebug
 mka installclean
 mka bacon
