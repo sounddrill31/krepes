25e36a232e8283f7130dbcb9bd4d8797622ce449
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-17 16:13:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 rm -rf .repo/local_manifests/  &&
 # Clone local_manifests repository
 repo init -u https://github.com/ProjectBlaze/manifest.git -b 12.1 --git-lfs --depth=1
-git clone https://github.com/nathannxx/local_manifest.git --depth 1 -b blaze .repo/local_manifests &&
+# Cloning Depis Teri
+git clone https://github.com/nathannxx/android_device_realme_RMX1941 --depth 1 -b twelve device/realme/RMX1941; \
+git clone https://github.com/P-Salik/android_kernel_realme_RMX1941 --depth 1 -b Q-rebase kernel/realme/RMX1941; \
+git clone https://github.com/P-Salik/android_vendor_realme_RMX1941 --depth 1 -b Sv2 vendor/realme/RMX1941; \
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
