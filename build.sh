af23c734d68cad32e2d39ba047e36a3e4649fca6
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-17 16:18:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ git clone https://github.com/nathannxx/android_device_realme_RMX1941 --depth 1 -
 git clone https://github.com/P-Salik/android_kernel_realme_RMX1941 --depth 1 -b Q-rebase kernel/realme/RMX1941; \
 git clone https://github.com/P-Salik/android_vendor_realme_RMX1941 --depth 1 -b Sv2 vendor/realme/RMX1941; \
 # Sync the repositories
-/opt/crave/resync.sh  && 
+/opt/crave/resync.sh; \
+source build/envsetup.sh; \
 # Set up build environment
 export BUILD_USERNAME=natehiggas
 export BUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 export WITH_GMS=true
-source build/envsetup.sh
  
 # Build the ROM
-lunch blaze_RMX1941-userdebug
+breakfast RMX1941 userdebug; \
 mka installclean
 mka bacon
