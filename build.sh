e7fff3b8d878233eba3af84d636d3251c318fb49
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-18 12:24:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ repo init -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next 
 git clone https://github.com/nathannxx/android_device_realme_RMX1941 --depth 1 -b twelve device/realme/RMX1941
 git clone https://github.com/P-Salik/android_kernel_realme_RMX1941 --depth 1 -b Q-rebase kernel/realme/RMX1941
 git clone https://github.com/P-Salik/android_vendor_realme_RMX1941 --depth 1 -b Sv2 vendor/realme/RMX1941
+git clone https://github.com/P-Salik/vendor_realme_RMX1941-ims.git --depth 1 -b lineage-20 vendor/realme/RMX1941-ims
 # Sync the repositories
 /opt/crave/resync.sh
 . build/envsetup.sh
