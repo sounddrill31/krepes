065f0676e0fbb18c38d01fc4cc03a7aa48fab90c
nathannxx <84331192+nathannxx@users.noreply.github.com>
2024-09-19 08:30:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install dialog bash sed wget git curl zip tar jq expect make cmake automake autoconf llvm lld lldb binutils bison perl gperf gawk flex bc python3 zstd openssl unzip cpio bc bison build-essential ccache liblz4-tool libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git libssl-dev dos2unix kmod python2 -y &&
 
 # Grabbing Bliss required Dependencies
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso xmlstarlet -y
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso xmlstarlet -y
 
 rm -rf .repo/local_manifests/  &&
 # Clone local_manifests repository
@@ -25,4 +25,4 @@ export TZ=Asia/Jakarta
  
 # Build the ROM
 echo 'BUILD STARTS NOW !!!'
-blissify -c RMX1941
+blissify -g -d RMX1941
