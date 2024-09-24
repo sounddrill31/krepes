b90a700b17e3447c363657628dca295f00d02ad3
Nathan Andrew <84331192+nathannxx@users.noreply.github.com>
2024-09-25 03:21:57 +0700

    Update ngising.sh

--- ngising.sh
+++ ngising.sh
@@ -4,12 +4,15 @@ repo init -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs
 git clone https://github.com/nathannxx/local_manifests_ngising --depth 1 -b ngising .repo/local_manifests && 
 
 # Sync the repositories
-/opt/crave/resync.sh  && 
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# resync
+repo sync --no-tags --no-clone-bundle --force-sync -j1 --fail-fast
 
 # Set up build environment
 export BUILD_USERNAME=nathannxx 
 export BUILD_HOSTNAME=YouDeer
-source build/envsetup.sh && 
+. build/envsetup.sh
 
 # Build the ROM
 lunch lineage_RMX1941-userdebug && make installclean && brunch RMX1941
