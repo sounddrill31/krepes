3c203a3b9aa1074c01bbfece73f6b53b3c846623
Nathan Andrew <84331192+nathannxx@users.noreply.github.com>
2024-09-26 22:00:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Initialize ROM and clone Device manifests
 echo ' Initialize ROM and Device manifests '
 rm -rf .repo/local_manifests/ &&
-repo init -u https://github.com/bananadroid/android_manifest.git -b 12
+repo init -u https://github.com/nathannxx/android_manifest.git -b 12
 git clone https://github.com/nathannxx/local_manifests_nss.git --depth 1 -b nss .repo/local_manifests
 
 # Sync the repositories
@@ -12,6 +12,7 @@ echo 'Setup Environment begins'
 . build/envsetup.sh
 
 # Build the ROM
+export BANANA_MAINTAINER=nathan:3
 export BUILD_USERNAME=nathannxx
 export BUILD_HOSTNAME=omkegams
 echo "BUILD STARTS NOW !!!"
