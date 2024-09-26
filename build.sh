b92175117bfd18d95399aa5669c3b4e3ad846565
Nathan Andrew <84331192+nathannxx@users.noreply.github.com>
2024-09-26 07:09:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Initialize ROM and clone Device manifests
 echo ' Initialize ROM and Device manifests '
 rm -rf .repo/local_manifests/ &&
-repo init -u ssh://git@github.com/NusantaraProject-ROM/android_manifest.git -b 12.1
+repo init --depth=1 -u https://github.com/Corvus-AOSP/android_manifest.git -b 12
 git clone https://github.com/nathannxx/local_manifests_nss.git --depth 1 -b nss .repo/local_manifests
 
 # Sync the repositories
 echo "Sync begin"
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 repo sync --no-tags --no-clone-bundle --force-sync -j1 --fail-fast
 echo 'Setup Environment begins'
 . build/envsetup.sh
@@ -15,4 +15,5 @@ echo 'Setup Environment begins'
 export BUILD_USERNAME=nathannxx
 export BUILD_HOSTNAME=omkegams
 echo "BUILD STARTS NOW !!!"
-mka nad -j
+lunch corvus_RMX1941-userdebug
+make corvus
