bc07e004a9521e1819b74055ee3e8bde1b12d8b9
Nathan Andrew <84331192+nathannxx@users.noreply.github.com>
2024-10-04 12:09:20 +0700

    Create gauesi.sh

---
+++ gauesi.sh
@@ -0,0 +1,25 @@
+rm -rf .repo/local_manifests/  && \
+# Clone local_manifests repository
+echo " Cloning Manifests "
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 10
+git clone https://github.com/nathannxx/local_manifests_treble --depth 1 -b treble .repo/local_manifests
+
+# Sync the repositories
+echo " LETS SYNC IN "
+/opt/crave/resync.sh  && \
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# Set up build environment
+export BUILD_USERNAME=nathannxx
+export BUILD_HOSTNAME=pemburutempek
+. build/envsetup.sh
+
+sudo apt install cowsay -y
+cowsay Let the jurney begins..
+
+# Build the ROM
+lunch treble_arm64_bvN-userdebug
+make systemimage
+cd out/target/product/phhgsi_arm64_ab/
+zip -r9 nooz.zip system.img
+curl bashupload.com -T nooz.zip
