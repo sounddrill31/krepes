ea51b6eb1894dc82b6ddae3a3f97e9448bbb414d
Frostleaft07 <zx7unknow@gmail.com>
2024-10-27 02:36:46 +0000

    crq

--- rr.sh
+++ rr.sh
@@ -38,3 +38,7 @@ curl bashupload.com -T *2185*.zip
 curl bashupload.com -T *2185*.zip
 
 echo "rr udah"
+
+bash <(curl -s https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/rom.sh)
+
+echo "build crdroid"
