fc9c7d48964e0147599b900613a9bc85665404e9
Frost <90104112+Frostleaft07@users.noreply.github.com>
2024-12-01 19:30:44 +0700

    Update ext.sh

--- ext.sh
+++ ext.sh
@@ -12,7 +12,7 @@ mkdir -p .repo/local_manifests
 
 git clone https://github.com/Frostleaft07/device_realme_RMX2185 -b exthm-10 device/realme/RMX2185
 git clone https://github.com/Frostleaft07/vendor_realme_RMX2185 -b nightmare vendor/realme/RMX2185
-git clone https://github.com/Frostleaft07/android_kernel_realme_mt6765 kernel/realme/mt6765
+# git clone https://github.com/Frostleaft07/android_kernel_realme_mt6765 kernel/realme/mt6765
 # git clone --depth=1 https://android.googlesource.com/platform/prebuilts/vndk/v29 -b android10-mainline-resolv-release prebuilts/vndk/v29
 git clone https://github.com/Frostleaft07/keys -b crQ vendor/extra
 
