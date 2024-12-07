59f4c770c4245d71c4a692c8219ed510fff1ba28
Frost <90104112+Frostleaft07@users.noreply.github.com>
2024-12-07 22:45:01 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
-sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io
+sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io -y
 mkdir rom ; cd rom ; wget https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/Dockerfile
 docker build -t otsu-builder .
