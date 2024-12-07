6b8c83552eef5aadc211a4088d3203d4c9a99c62
Frost <90104112+Frostleaft07@users.noreply.github.com>
2024-12-08 05:01:47 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
-sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io -y
+sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io docker-buildx-plugin -y
+docker buildx install
 mkdir rom ; cd rom ; wget https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/Dockerfile
 docker buildx build .
