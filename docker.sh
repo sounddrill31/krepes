5554532c8c0dbd913a41e305ed55313c88fd600b
Frost <90104112+Frostleaft07@users.noreply.github.com>
2024-12-08 05:17:29 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,15 @@
-sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io docker-buildx-plugin -y
+sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io ca-certificates curl gnupg -y
 docker buildx install
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install -y docker-buildx-plugin
 mkdir rom ; cd rom ; wget https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/Dockerfile
 docker buildx build .
