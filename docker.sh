14996eeb1030580b1dfe58576deff9e899c2d1a3
Frost <90104112+Frostleaft07@users.noreply.github.com>
2024-12-08 07:08:11 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 sudo apt update -y ; sudo apt upgrade -y ; sudo apt install docker docker.io ca-certificates curl gnupg -y
 wget https://github.com/docker/buildx/releases/download/v0.19.2/buildx-v0.19.2.linux-amd64
 mv *buildx* /bin/buildx
+sudo chmod +x /bin/buildx
 mkdir rom ; cd rom ; wget https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/Dockerfile
 buildx build .
