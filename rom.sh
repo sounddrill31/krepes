765f3ec0b4ac0fb0ba023fd402c4796ef5c47a4f
Frostleaft07 <zx7unknow@gmail.com>
2024-12-14 05:49:50 +0700

    los

--- rom.sh
+++ rom.sh
@@ -3,16 +3,12 @@ sudo apt-get update -y && sudo apt-get install dialog bash sed wget git curl zip
 rm -rf .repo/local_manifests/
 find . -delete
 
-repo init -u https://github.com/Frostleaft07/android -b 10.0 --depth=1
-
-mkdir -p .repo/local_manifests
-wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/Frostleaft07/krepes/refs/heads/main/roomservice.xml
+repo init -u https://github.com/ForkLineageOS/android -b lineage-18.1 --depth=1
 
 /opt/crave/resync.sh ; repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
-git clone https://github.com/Frostleaft07/device_realme_RMX2185 -b nightmare device/realme/RMX2185
-git clone https://github.com/Frostleaft07/vendor_realme_RMX2185 -b nightmare vendor/realme/RMX2185
+git clone https://github.com/Frostleaft07/device_realme_RMX2185 -b exthm-11 device/realme/RMX2185
+git clone https://github.com/Frostleaft07/vendor_realme_RMX2185 -b N11 vendor/realme/RMX2185
 git clone https://github.com/Frostleaft07/android_kernel_realme_mt6765 kernel/realme/mt6765
 # git clone --depth=1 https://android.googlesource.com/platform/prebuilts/vndk/v29 -b android10-mainline-resolv-release prebuilts/vndk/v29
 git clone https://github.com/Frostleaft07/keys -b crQ vendor/extra
@@ -20,8 +16,10 @@ git clone https://github.com/Frostleaft07/keys -b crQ vendor/extra
 ls build
 source build/env*.sh
 
-rm -rf packages/apps/ManagedProvisioning
-git clone --depth=1 https://android.googlesource.com/platform/packages/apps/ManagedProvisioning -b android10-release packages/apps/ManagedProvisioning
+#rm -rf packages/apps/ManagedProvisioning
+#git clone --depth=1 https://android.googlesource.com/platform/packages/apps/ManagedProvisioning -b android10-release packages/apps/ManagedProvisioning
+rm -rf packages/providers/DownloadProvider/
+git clone https://android.googlesource.com/platform/packages/providers/DownloadProvider -b android11-release packages/providers/DownloadProvider
 
 otsu=$(pwd)
 depis=device/realme/RMX2185
@@ -35,10 +33,6 @@ brunch RMX2185
 
 cd out/target/product/RMX2185/
 
-curl bashupload.com -T crDroidAndroid-10.0-*2185*.zip
+curl bashupload.com -T *-*2185*.zip
 
-curl bashupload.com -T crDroidAndroid-10.0-*2185*.zip
-
-curl bashupload.com -T crDroidAndroid-10.0-*2185*.zip
-
-curl bashupload.com -T crDroidAndroid-10.0-*2185*.zip
+curl bashupload.com -T *-*2185*.zip
